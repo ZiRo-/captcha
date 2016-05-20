78241520fe3470ab1db28c6c3d5a106af80ad6a5
ZiRo <z_i_r_o@ymail.com>
2016-05-20 15:34:29 +0200

    add font generator

---
+++ fontgen/fontgen.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"os"
+	"os/exec"
+	"encoding/gob"
+	"fmt"
+	"image"
+	"flag"
+	"log"
+	"io"
+	 _ "image/gif"
+	 _ "image/png"
+	 _ "image/jpeg"
+)
+
+
+var thresh int = 200;
+
+var font = flag.String("font", "Monospace", "Font name")
+
+func main() {
+	flag.Parse()
+	fname := flag.Arg(0)
+	if fname == "" {
+		usage()
+		os.Exit(1)
+	}
+	fm := make(map[rune][]byte)
+	
+	for c := '0'; c <= '9'; c++ {
+		fm[c]=genChar(c)
+	}
+	for c := 'a'; c <= 'z'; c++ {
+		fm[c]=genChar(c)
+	}
+	for c := 'A'; c <= 'Z'; c++ {
+		fm[c]=genChar(c)
+	}
+	
+	file, err := os.Create(fname)
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	enc := gob.NewEncoder(file)
+	err = enc.Encode(fm)
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	file.Close()
+}
+
+
+func convertImage(file io.Reader) []byte {
+	m, _, err := image.Decode(file)
+	
+	if err != nil {
+		log.Fatal(err)
+	}
+	bounds := m.Bounds()
+	xp := bounds.Max.X - bounds.Min.X
+	yp := bounds.Max.Y - bounds.Min.Y
+	a := make([]byte, yp*xp)
+	
+	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+		for x := bounds.Min.X; x < bounds.Max.X; x++ {
+			col := m.At(x, y)
+			r, g, b, _ := col.RGBA()
+			gr := (299*r + 587*g + 114*b + 500) / 1000
+			gr>>=8
+			
+			px := 0
+			if int(gr) <= thresh {
+				px =1
+			}
+			idx := xp* (y-bounds.Min.Y) + x-bounds.Min.X
+			a[idx]=byte(px)
+		}
+	}
+	return a
+}
+
+func genChar(char rune) []byte {
+	c := fmt.Sprintf("%c", char)
+	cmd := genImage(c, *font)
+	
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := cmd.Start(); err != nil {
+		log.Fatal(err)
+	}
+	
+	a := convertImage(stdout)
+	
+	if err := cmd.Wait(); err != nil {
+		log.Fatal(err)
+	}
+	return a
+}
+
+func genImage(letter, font string) *exec.Cmd {
+	return exec.Command("convert", "-size", "11x18", "-background", "white", "-fill", "black", "-filter", "Point", "-font", font, "-gravity", "center", "label:"+letter, "gif:-")
+}
+
+func usage() {
+	fmt.Fprintf(os.Stderr, "usage: fontgen [flags] filename\n")
+	flag.PrintDefaults()
+}
