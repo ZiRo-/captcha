f6a26daca4716a3f8238d0904fb0e555985e31ff
ZiRo <z_i_r_o@ymail.com>
2016-05-22 15:15:10 +0200

    advertise fonts

--- captcha.go
+++ captcha.go
@@ -3,7 +3,7 @@
 // license that can be found in the LICENSE file.
 
 // Package captcha implements generation and verification of image
-// CAPTCHAs.
+// CAPTCHAs with custom fonts.
 //
 // A captcha solution is the sequence of digits 0-9 and letters a-z, A-Z 
 // with a defined length.
